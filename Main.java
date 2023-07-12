096bb4998a8f3c26504094961e29b8bafa4d77f2
nurdin kurniawan <132554292+Nurdin16@users.noreply.github.com>
2023-07-12 17:11:08 +0700

    Main.java

--- Main.java
+++ Main.java
@@ -9,6 +9,8 @@ public class Main {
         Peminjam peminjam1 = new Mahasiswa();
         peminjam1.setNamaPeminjam("Huda");
         peminjam1.setKodePeminjam("M001");
+
+        
         
        peminjam1.doLogin();
        //peminjam1.doLogout();
@@ -20,10 +22,32 @@ public class Main {
         System.out.println("Kode Peminjam :"+peminjaman1.getPeminjam().getKodePeminjam());
         System.out.println("Nama Petugas :"+peminjaman1.getPetugas().getNamaPetugas());
         System.out.println("Nama Buku :"+peminjaman1.getBuku().getNamaBuku());
+        Denda denda1 = new Denda(peminjaman1, 6);
+        System.out.println("Total Denda :"+denda1.hitungDenda());
+        
+
+        System.out.println("=====================================");
+
+        Peminjaman peminjaman2 = new Peminjaman(peminjam1, petugas1, buku2);
+
+        System.out.println("Nama Peminjam :"+peminjaman2.getPeminjam().getNamaPeminjam());
+        System.out.println("Kode Peminjam :"+peminjaman2.getPeminjam().getKodePeminjam());
+        System.out.println("Nama Petugas :"+peminjaman2.getPetugas().getNamaPetugas());
+        System.out.println("Nama Buku :"+peminjaman2.getBuku().getNamaBuku());
+        Denda denda2 = new Denda(peminjaman2, 7);
+        System.out.println("Total Denda :"+denda2.hitungDenda());
+     
+
+        
+
+        
+
         }catch (Exception e){
             System.out.println("Terjadi Kesalahan"+e.getMessage());
         } finally {
             System.out.println("Program Selesai");
         }
+
+        
     }
 }
